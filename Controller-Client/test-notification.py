665ed1f3a1c78c79e36df4a4178b423965939b7c
ibrezm1 <ibrezm1@gmail.com>
2021-08-14 21:14:57 +0530

    test

--- Controller-Client/test-notification.py
+++ Controller-Client/test-notification.py
@@ -1,11 +1,4 @@
-# TODO :
-# Setup venv : https://www.pyimagesearch.com/2018/09/19/pip-install-opencv/
-# Get image png :  http://192.168.1.13:5000/getimage?cx=1147&cy=31&ch=35&cw=35
-# Scan Notification  :
-# Above threshhold
-# Notify : https://askubuntu.com/questions/616985/how-do-i-send-desktop-notifications-using-python-3
 import time
-import urllib.request
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
@@ -14,11 +7,31 @@ import requests
 from argparse import ArgumentParser
 import os.path
 import yaml
+import logging
+import sys
+
+import signal
+import sys
+
+from operator import xor
+
+def signal_handler(signal, frame):
+    # Handle any cleanup here
+    print('SIGINT or CTRL-C detected. Exiting gracefully')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+
 
 def url_to_image(url):
-    # download the image, convert it to a NumPy array, and then read
-    # it into OpenCV format
-    # https://stackoverflow.com/questions/57539233/how-to-open-an-image-from-an-url-with-opencv-using-requests-from-python
     resp = requests.get(url, stream=True).raw
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv.imdecode(image, cv.IMREAD_COLOR)
@@ -31,6 +44,38 @@ def is_valid_file(parser, arg):
         return open(arg, 'r')  # return an open file handle
 
 
+class Bot:
+    def __init__(self, url,threshold):
+        self.serverurl = url
+        self.threshold = threshold
+
+    def botObserve(self,region,image):
+        self.region = region
+        ex_x,ex_y,ex_w,ex_h = region
+        img_rgb = url_to_image(f"{self.serverurl}/getimage?cx={ex_x}&cy={ex_y}&cw={ex_w}&ch={ex_h}")
+
+        img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+        template = cv.imread(image,0)
+        w, h = template.shape[::-1]
+        self.res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(self.res)
+        return max_val>=self.threshold
+
+
+    def botNotify(self):
+        requests.get(f"{self.serverurl}/play")
+
+    def botAct(self):
+        ex_x,ex_y,ex_w,ex_h = self.region
+        loc = np.where( self.res >= self.threshold)
+        for pt in zip(*loc[::-1]):
+            rl_x, rl_y = ( pt[0] + int(w/2), pt[1] + int(h/2) )
+            cx_x, cx_y = ex_x + rl_x,ex_y + rl_y
+            cur_time = time.strftime("%m/%d/%Y, %H:%M:%S")
+            print(f"Found OK {cx_x} {cx_y} at {cur_time}")
+            requests.get(f"{self.serverurl}/mouse?cx={cx_x}&cy={cx_y}")
+
+   
 
 def main():
     parser = ArgumentParser(description="Config Selection")
@@ -38,47 +83,19 @@ def main():
                         help="input file with config.yaml", metavar="FILE",
                         type=lambda x: is_valid_file(parser, x))
     args = parser.parse_args()
-    config = args.filename
+    config = args.filename.name
+    
     with open(config, 'r') as f:
-        doc = yaml.safe_load(f)
+        conf = yaml.safe_load(f)
 
+    bot1 = Bot(conf['server'], conf['threshold'])
     while True:
-        #urllib.request.urlretrieve(f"http://192.168.1.13:5000/getimage?cx=1147&cy=31&ch=35&cw=35", 'images/notify.png')
-        #img_rgb = cv.imread('images/notify.png')
-        ex_x,ex_y,ex_w,ex_h = (761,253,216,114)
-
-        img_rgb = url_to_image(f"http://192.168.1.13:5000/getimage?cx={ex_x}&cy={ex_y}&cw={ex_w}&ch={ex_h}")
-
-        # img_rgb = cv.imread('images/bell-nonotify.png')
-        # img_rgb = cv.imread('images/bell-notify.png')
-        img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-        template = cv.imread('images/notify-red.png',0)
-        w, h = template.shape[::-1]
-        res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-        threshold = 0.8
-
-        # verdict = 'found' if max_val>=threshold else 'not found'
-        # print(verdict)
-
-        # x = urllib.request.urlopen('http://192.168.1.13:5000/play')
-        # print(x.read())
-
-        if max_val>=threshold:
-            s.call(['notify-send','foo','bar'])
-            urllib.request.urlopen('http://192.168.1.13:5000/play')
-        time.sleep(3)
-        # loc = np.where( res >= threshold)
-        # points = zip(*loc[::-1])
-        # for pt in zip(*loc[::-1]):
-        #     cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (255,0,0), 2)
-        # cv.imwrite('res.png',img_rgb)
-        # cv.imshow('final',img_rgb)
-
-        # cv.waitKey(0) 
-        
-        # #closing all open windows 
-        # cv.destroyAllWindows() 
+        for regions in conf['monitor']['points']:
+            if not xor(regions['match']=='match' , bot1.botObserve(regions['region'],regions['matchimage'])):
+                if regions['notify'] == 'true' : bot1.botNotify()   
+                if regions['click'] == 'true' : bot1.botAct()                
+        time.sleep(conf['delay'])
 
 if __name__ == "__main__":
     main()
+
