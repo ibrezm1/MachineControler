cb0222a5a532efa31186e5add6cc822a46225aaa
ibrezm1 <59130871+ibrezm1@users.noreply.github.com>
2021-08-15 11:38:56 +0530

    Update setup.bat

--- To-Control-Win-Server/setup.bat
+++ To-Control-Win-Server/setup.bat
@@ -1,3 +1,4 @@
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe', 'plink.exe') }"
 python3 -m venv .\tvent
 call tvent\Scripts\activate.bat
 pip3 install -r requirements.txt
