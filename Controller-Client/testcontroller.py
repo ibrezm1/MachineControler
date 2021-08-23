b939c799a5097ea79ae4aae44fd37736bb63f242
ibrezm1 <ibrezm1@gmail.com>
2021-08-23 22:52:03 +0530

    added activity test

--- Controller-Client/testcontroller.py
+++ Controller-Client/testcontroller.py
@@ -12,14 +12,14 @@ def test_botObserve(mocker):
         'controller.url_to_image',
         return_value=cv.imread('images/notify.png')
     )
-    bot1 = Bot('dummyserver', 0.8)
+    bot1 = Bot('dummyserver', 0.8, 60)
     expected = True
     actual = bot1.botObserve((1,2,3,4),'images/notify.png')
     assert expected == actual
     
 def test_notify(requests_mock):
     requests_mock.get('http://test.com', text='data')
-    bot1 = Bot('dummyserver', 0.8,60)
+    bot1 = Bot('dummyserver', 0.8, 60)
     expected = 'data'
     actual = requests.get('http://test.com').text
     assert expected == actual
@@ -32,7 +32,7 @@ def test_botAct(mocker,requests_mock):
         'controller.url_to_image',
         return_value=cv.imread('images/notify.png')
     )
-    bot1 = Bot('dummyserver', 0.8,60)
+    bot1 = Bot('dummyserver', 0.8, 60)
     expected = True
     actual = bot1.botObserve((1,2,3,4),'images/notify.png')
     assert expected == actual
